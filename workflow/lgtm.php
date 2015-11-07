49aa66e7e8a1bebfee0d25fc09e375b951839941
Yuta Mizui <me@mziyut.com>
2015-11-07 15:35:29 +0900

    delete cache process

--- workflow/lgtm.php
+++ workflow/lgtm.php
@@ -5,44 +5,30 @@ $wf = new Workflows();
 # GET LGTM JSON
 $url = "http://www.lgtm.in/g";
 $opts = array(
-	'http'=>array(
-		'method' => "GET",
-		'header' => "Accept:application/json, text/javascript"
-	)
+    'http'=>array(
+        'method' => "GET",
+        'header' => "Accept:application/json, text/javascript"
+    )
 );
 $context = stream_context_create($opts);
 $json = json_decode(file_get_contents($url, false, $context));
 
-# Get LGTM Image
-$image_dir = './thumb';
-$image_url = $json->actualImageUrl;
-$image_type = substr($image_url, strrpos($image_url, '.') + 1);
-$image_path = $image_dir.'/icon.'.$image_type;
-
-# Delete old images
-$res_dir = opendir( $image_dir );
-while( $file_name = readdir($res_dir)) {
-	if(is_file($image_dir.'/'.$file_name)) {
-		unlink($image_dir.'/'.$file_name);
-	}
-}
-
 # Markdown's image syntax
 $wf->result(
-	time(),
-	"![{$json->imageUrl}]({$json->imageUrl})",
-	"Copy Markdown's syntax to Clipboard",
-	"Good:{$json->likes}  Bad:{$json->dislikes}",
-	$image_path
+    time(),
+    "![{$json->imageUrl}]({$json->imageUrl})",
+    "Copy Markdown's syntax to Clipboard",
+    "Good:{$json->likes}  Bad:{$json->dislikes}",
+    $image_path
 );
 
 # Raw image url
 $wf->result(
-	time(),
-	$json->imageUrl,
-	"Copy raw image url to Clipboard",
-	"Good:{$json->likes}  Bad:{$json->dislikes}",
-	$image_path
+    time(),
+    $json->imageUrl,
+    "Copy raw image url to Clipboard",
+    "Good:{$json->likes}  Bad:{$json->dislikes}",
+    $image_path
 );
 echo $wf->toxml();
 
